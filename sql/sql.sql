7a333314f676237d4edf62a704953c3d1bf83d22
Evan Lazaro <evanlazaro@gmail.com>
2019-10-30 20:10:53 -0400

    Update DB structure, add sample data

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,29 +1,36 @@
-
 CREATE TABLE organization (
       orgID varchar(20) PRIMARY KEY,
+      orgName varchar(100),
       description  varchar(100),
-      website varchar(100)
-
+      website varchar(100),
+      adminID varchar(20),
+      FOREIGN KEY(adminID) REFERENCES users(rcsID)
 );
+
 CREATE TABLE users (
       rcsID varchar(20) PRIMARY KEY,
-      age   INTEGER,
-      role  varchar(20),
-      orgID varchar(20),
-      FOREIGN KEY(orgID) REFERENCES organization(orgID)
+      fullname varchar(100),
+      about  varchar(100),
+);
 
+CREATE TABLE userorganization (
+      orgID varchar(20),
+      rcsID  varchar(20),
+      PRIMARY KEY (rcsID, orgID),
+      FOREIGN KEY(orgID) REFERENCES events(orgID),
+      FOREIGN KEY(rcsid) REFERENCES users(rcsID)
 );
 
 CREATE TABLE events (
-      eventID varchar(20) PRIMARY KEY,
+      eventID Integer(20) PRIMARY KEY AUTO_INCREMENT,
+      eventname varchar(100),
       orgID varchar(20),
       description  varchar(100),
-      place   varchar(100),
+      location   varchar(100),
       occupancy INTEGER,
-      eventlength INTEGER,
+      eventduration INTEGER,
       eventdate datetime,
       FOREIGN KEY(orgID) REFERENCES organization(orgID)
-
 );
 
 CREATE TABLE userevent (
@@ -32,5 +39,4 @@ CREATE TABLE userevent (
       PRIMARY KEY (rcsID, eventID),
       FOREIGN KEY(eventID) REFERENCES events(eventID),
       FOREIGN KEY(rcsid) REFERENCES users(rcsID)
-
 );
